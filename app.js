52e76563c0b8e183e1eff64769394e9cdf35c32d
darryl22 <darrylandrew22@gmail.com>
2025-03-04 23:31:28 +0300

    cart update

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express")
 const app = express()
 require("dotenv").config()
 const bodyParser = require("body-parser")
+const Mailjet = require('node-mailjet');
+const mailjet = Mailjet.apiConnect(
+    process.env.MAILJET_API_KEY,
+    process.env.MAILJET_API_SECRET,
+);
 const {MongoClient, ObjectId} = require("mongodb")
 const session = require("express-session")
 const MongoDBStore  = require("connect-mongodb-session")(session)
@@ -30,16 +35,17 @@ app.use(require("express-session")({
 app.use((request, response, next) => {
     if (request.session.cart === undefined) {
         request.session.cart = []
+        request.session.cartTotal = 0
     }
     next()
 })
 
 app.get("/", (request, response) => {
-    response.render("index.ejs", {cart: request.session.cart})
+    response.render("index.ejs", {cart: request.session.cart, cartTotal: request.session.cartTotal})
 })
 
 app.get("/aboutus", (request, response) => {
-    response.render("aboutus.ejs", {cart: request.session.cart})
+    response.render("aboutus.ejs", {cart: request.session.cart, cartTotal: request.session.cartTotal})
 })
 
 app.get("/shop/:category", (request, response) => {
@@ -48,18 +54,16 @@ app.get("/shop/:category", (request, response) => {
             const database = client.db("lamago")
             const clothing = database.collection("clothing")
             let res = await clothing.find().toArray()
-            response.render("shop.ejs", {items: res, cart: request.session.cart})
+            response.render("shop.ejs", {items: res, cart: request.session.cart, cartTotal: request.session.cartTotal})
         } catch (error) {
             console.log(error)
-            response.render("shop.ejs", {cart: request.session.cart})
+            response.render("shop.ejs", {cart: request.session.cart, cartTotal: request.session.cartTotal})
         }
     }
     db()
 })
 
 app.get("/itemdetail/:id", (request, response) => {
-    console.log(request.session)
-    // request.session.cart = []
 
     let id = new ObjectId(request.params.id)
     async function db() {
@@ -67,10 +71,10 @@ app.get("/itemdetail/:id", (request, response) => {
             const database = client.db("lamago")
             const item = database.collection("clothing")
             let res = await item.findOne({"_id": id})
-            response.render("itemdetail.ejs", {item: res, cart: request.session.cart})
+            response.render("itemdetail.ejs", {item: res, cart: request.session.cart, cartTotal: request.session.cartTotal})
         } catch (error) {
             console.log(error)
-            response.render("itemdetail.ejs", {cart: request.session.cart})
+            response.render("itemdetail.ejs", {cart: request.session.cart, cartTotal: request.session.cartTotal})
         }
     }
     db()
@@ -78,36 +82,117 @@ app.get("/itemdetail/:id", (request, response) => {
 
 app.get("/cart", (request,response) => {
     console.log("cart function")
-    response.render("cart.ejs")
+    response.render("cart.ejs", {cart: request.session.cart, cartTotal: request.session.cartTotal})
 })
 
 app.post("/cart", (request,response) => {
-    console.log(request.session)
-    console.log(request.body)
     let action = request.body.action
+    console.log(request.body)
     let cart = request.session.cart
-    let item = {
-        size: request.body.size,
-        quantity: parseInt(request.body.quantity),
-        itemname: request.body.itemname,
-        price: request.body.price,
-        image: request.body.image
-    }
     if (action === "addone") {
+        let item = {
+            size: request.body.size,
+            quantity: parseInt(request.body.quantity),
+            itemname: request.body.itemname,
+            price: request.body.price,
+            image: request.body.image
+        }
+
         if (cart.length === 0) {
             cart.push(item)
+            console.log("first item")
         } else {
+            let inserted = false
             for (let x = 0; x < cart.length; x++) {
                 if (cart[x].itemname === item.itemname) {
-                    cart[x].quantity = cart[x].quantity + 1
+                    cart[x].quantity = parseInt(cart[x].quantity) + parseInt(request.body.quantity)
+                    console.log("added quantity")
+                    inserted = true
                 }
-                if ((x + 1) === cart.length && cart[x].itemname !== item.itemname) {
-                    cart.push(item)
-                }
+            }
+            if (inserted === false) {
+                cart.push(item)
+            }
+        }
+    } else if (action === "removeone") {
+        for (let x = 0; x < cart.length; x++) {
+            if (cart[x].itemname === request.body.itemname) {
+                cart[x].quantity = parseInt(cart[x].quantity) - 1
+                console.log("remove one")
+                console.log(cart[x].quantity)
+            }
+        }
+    } else if (action === "addquantity") {
+        for (let x = 0; x < cart.length; x++) {
+            if (cart[x].itemname === request.body.itemname) {
+                cart[x].quantity = parseInt(cart[x].quantity) + 1
+                console.log("added one")
+                console.log(cart[x].quantity)
+            }
+        }
+    } else if (action === "removeitem") {
+        for (let x = 0; x < cart.length; x++) {
+            if (cart[x].itemname === request.body.itemname) {
+                cart.splice(x, 1)
+                console.log("removed item")
             }
         }
     }
-    response.json({"success": true, "cart": request.session.cart})
+    let temp = 0
+    for (let x = 0; x < cart.length; x++) {
+        temp = temp + (cart[x].price * cart[x].quantity)
+    }
+    console.log(temp)
+    request.session.cartTotal = temp
+    response.json({"success": true, "cart": request.session.cart, "cartTotal": request.session.cartTotal})
+})
+
+app.get("/login", (request, response) => {
+    response.render("login.ejs")
+})
+
+app.post("/login", (request, response) => {
+    const email = request.body.email
+    const mailrequest = mailjet.post('send', { version: 'v3.1' }).request({
+        Messages: [
+            {
+                From: {
+                    Email: "lamagostoremail@gmail.com",
+                    Name: "Mailjet Pilot"
+                    },
+                To: [
+                    {
+                    Email: email,
+                    Name: "passenger 1"
+                    }
+                ],
+                Subject: "Lamago Login Verification",
+                TextPart: "New token",
+                HTMLPart: `<p>Your login token is : <span style='font-weight: bold;'>123456</span></p>`
+            }
+            ]
+        })
+
+        mailrequest
+            .then((result) => {
+                console.log(result)
+            })
+            .catch((err) => {
+                console.log(err.statusCode)
+            })
+    response.redirect(`/verification?email=${email}`)
+})
+
+app.get("/verification", (request, response) => {
+    response.render("verification.ejs")
+})
+
+app.post("/verification", (request, response) => {
+    response.redirect("/profile")
+})
+
+app.get("/profile", (request, response) => {
+    response.render("profile.ejs", {cart: request.session.cart, cartTotal: request.session.cartTotal})
 })
 
 app.listen(PORT, () => {
